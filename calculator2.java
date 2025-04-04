6f533fceebf32ac381e54b870a1e0e2a025ac893
veeres-javali18 <veeresjavali038@gmail.com>
2025-04-04 10:31:41 +0530

    Save this file

---
+++ calculator2.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public class calculator1 {
+    public static void main(String[] args)
+    {
+       int a=10;
+       int b=20;
+       System.out.println("Addition is"+(a+b)); 
+       System.out.println("substraction is"+(a-b)); 
+       System.out.println("Multiplication is"+(a*b)); 
+       System.out.println("Division is"+(a/b)); 
+       System.out.println("Modulous is"+(a%b)); 
+       System.out.println("Arithmetic operations");
+       
+    }
+    
+}
+
+    
+
+
